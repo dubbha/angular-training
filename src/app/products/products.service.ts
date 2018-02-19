7d5a0028fb48af0203764507f0955bbcaf6949bf
Oleksiy Dubovyk <oleksiy.dubovyk@racingpost.com>
2018-02-19 18:49:34 +0200

    [task5] progress

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -6,9 +6,9 @@ import { Category } from './product/product.enum';
 const initProducts = [
   new Product(1, 'Liebherr Fridge', 'Cools your food', 800, true, Category.Household, ['metal', 'cooler'], [2]),
   new Product(2, 'Siemens Fridge', 'Cools your food', 750, true, Category.Household, ['metal', 'cooler'], [1]),
-  new Product(3, 'TV 40 inches', 'Washes your brain', 300, true, Category.Electonics, ['plastic', 'glass'], [4, 5]),
-  new Product(4, 'TV 50 inches', 'Washes your brain', 320, true, Category.Electonics, ['plastic', 'glass'], [3, 5]),
-  new Product(5, 'TV 55 inches', 'Washes your brain', 320, false, Category.Electonics, ['plastic', 'glass'], [3, 4]),
+  new Product(3, 'TV 40 inches', 'Washes your brain', 300, true, Category.Electronics, ['plastic', 'glass'], [4, 5]),
+  new Product(4, 'TV 50 inches', 'Washes your brain', 320, true, Category.Electronics, ['plastic', 'glass'], [3, 5]),
+  new Product(5, 'TV 55 inches', 'Washes your brain', 320, false, Category.Electronics, ['plastic', 'glass'], [3, 4]),
 ];
 
 @Injectable()
@@ -65,6 +65,6 @@ export class ProductService {
   getNextId() {
     const arr = [...this.products];   // sort() would sort in place, make a copy
     arr.sort((a, b) => a.id - b.id);
-    return arr.pop().id + 1;// next free ID
+    return arr.pop().id + 1;  // next free ID
   }
 }
